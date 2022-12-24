1b734ca86ba3bbb3c31fdb390aaa7747841a3eb7
Stephen L. McMahon <stephen@slmcmahon.com>
2022-12-24 08:36:19 -0600

    Updated to get the latest commit for the given repo and add one second to the date since it is inclusive

--- loadstats.py
+++ loadstats.py
@@ -3,6 +3,7 @@ import sys
 import sqlite3
 import re
 import subprocess
+from datetime import datetime, timedelta
 
 DEFAULT_DB_PATH = os.path.join(os.path.expanduser('~'), 'commits.sqlite3')
 COMMITS_TABLE = """create table if not exists commits (
@@ -66,6 +67,18 @@ def get_db_path():
         return actual_path
 
 
+def get_last_modified(conn, repo_name):
+    csr = conn.cursor()
+    csr.execute(
+        "select max(committed_on) from commits where repo_name = ?", (repo_name,))
+    last_mod = csr.fetchone()[0]
+    if not last_mod:
+        return None
+    else:
+        dt = datetime.strptime(last_mod, '%Y-%m-%dT%H:%M:%S')
+        return dt + timedelta(seconds=1)
+
+
 def main():
     repo_name = get_repo_name()
     if not repo_name:
@@ -85,28 +98,38 @@ def main():
 
     create_tables(cursor)
 
-    log_data = subprocess.Popen(["git", "log", "--pretty=format:|%h||%s||%an||%ae||%aI", "--numstat"],
-                              stdout=subprocess.PIPE, encoding="utf-8")
+    last_modified = get_last_modified(conn, repo_name)
+    procArgs = ["git", "log", "--date=local",
+                "--pretty=format:|%h||%s||%an||%ae||%aI", "--numstat"]
+    if last_modified:
+        # if last_modified actually contains a date, then we want to add new records
+        # after that date, so we insert that arg here.
+        procArgs.insert(3, f'--after={last_modified}')
+
+    # print(f'Executing: {" ".join(procArgs)}')
+    log_data = subprocess.Popen(
+        procArgs, stdout=subprocess.PIPE, encoding="utf-8")
 
     commit = None
     for line in log_data.stdout.readlines():
         line = line.strip()
-        
+
         if len(line) == 0:
             continue
-        
+
         if line[0] == '|':
             m = commit_pattern.match(line)
             commit = m[1]
-            print(f'Writing data for {commit}.')
-            add_commit(cursor, commit, repo_name, m[2], m[3], m[4], m[5])
+            print(f'Writing data for {commit} for {repo_name}.')
+            ts = m[5][:-6]
+            add_commit(cursor, commit, repo_name, m[2], m[3], m[4], ts)
         else:
             m = file_pattern.match(line)
             add_commit_file(cursor, commit, m[3], m[1], m[2])
-
+        
+    print(f'Database is up to date for {repo_name}.')
     conn.commit()
     conn.close()
-    print(f'Created or updated database at {db_path}')
 
 
 if __name__ == "__main__":
